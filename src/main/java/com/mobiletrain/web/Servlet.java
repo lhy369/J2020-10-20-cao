83000cdb9c3b22e02853ee3984b63406f047e169
lhy369 <73153341+lhy369@users.noreply.github.com>
2020-10-20 22:41:11 +0800

    Update Servlet.java

--- src/main/java/com/mobiletrain/web/Servlet.java
+++ src/main/java/com/mobiletrain/web/Servlet.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 @WebServlet("/Servlet")
 public class Servlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+//啥也不是
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
